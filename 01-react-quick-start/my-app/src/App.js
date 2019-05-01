e778a53eb8376d0c0926bb7add89081c0c06732b
itclancode <itclancode@163.com>
2019-05-02 01:09:51 +0800

    feature-whatComponent 【React快速学习】什么是组件,如何定义组件

--- 01-react-quick-start/my-app/src/App.js
+++ 01-react-quick-start/my-app/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
-function App() {
+import React, { Component } from 'react';
+// 原始的写法
+/*function App() {
     return (
         <div className="App">
             Learn React
         </div>
 );
+}*/
+// 定义react组件,定义来App这样的一个类,它继承于一个Component的类
+/**
+ * 一个React组件里面必须要有render函数，这个函数负责这个组件要显示的内容,retrurn出去的内容,就是这个组件要显示的内容
+ * export default 导出与导入时配对使用的
+ * */
+class App extends Component { // 等同于class App extends React.component {}的写法
+    render() {
+        return (
+            <div className="App">
+                Learn React
+            </div>
+        )
+    }
 }
 export default App;
--- 01-react-quick-start/my-app/src/index.js
+++ 01-react-quick-start/my-app/src/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/**
+ * @Author itclancoder
+ * @Descortaion react入口文件
+ * @Date 2019/5/2
+ * @param
+ * @return
+ *
+ * App就是一个组件,在react中以大写字母开头的,都是组件
+ * RectDom会把App这个组件渲染到后面的root标签里面去,加载一个组件,并把组件渲染到页面上
+* */
+
+import React from 'react'; // 加载react这个库,识别react的语法(例如首字母大写),识别jsx语法
+import ReactDOM from 'react-dom'; // ReactDom可以让一个组件挂载到页面的一个节点上
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
