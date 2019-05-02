ec67d103599d833391ee43614a4931ebaae26ce9
itclancode <itclancode@163.com>
2019-05-03 03:10:14 +0800

    feature-writeTodoList 【快速学习react】编写todolist

--- 01-react-quick-start/my-app/src/App.js
+++ 01-react-quick-start/my-app/src/App.js
@@ -3,14 +3,13 @@
  * export default 导出与导入时配对使用的 refactor
  * */
 import React, { Component } from 'react';
-import HelloMessage from  './HelloMessage';  // 引入HelloMessage组件
+import TodoList from  './TodoList';  // 引入TodoList组件
 // 定义react组件,定义来App这样的一个类,它继承于一个Component的类
 class App extends Component {
     render() {
         return (
             <div className="App">
-                 <HelloMessage name="itclancoder" />
-                 这是一个HelloMessage组件
+                 <TodoList />
             </div>
         )
     }
---
+++ 01-react-quick-start/my-app/src/TodoList.js
@@ -0,0 +1,45 @@
+/**
+ * @Author itclancoder
+ * @Descortaion TodoList
+ * @Date 2019/5/2
+ * @param
+ * @return
+ * this指向问题,指向的是button本身,怎么让里面的this指向最外层的那个TodoList本身
+ *
+ * */
+import React from 'react';
+class TodoList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            list: []
+        }
+    }
+    handleBtnClick() {
+        // 这里的this其实是button按钮,在react中不要直接的去更改state的数据,应当使用this.setState的语法
+        this.setState({
+           list: [ ...this.state.list, 'hello world']
+        });
+    }
+    render() {
+        return (
+            <div>
+                <div>
+                    <input />
+                    <button onClick={this.handleBtnClick.bind(this)}>add</button>
+                </div>
+                <div>
+                    <ul>
+                        {
+                            this.state.list.map(function(item, index) {
+                                return <li key={index}>{item}</li>
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
